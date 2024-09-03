078cf7d7e514a094bc9ac21d7cde5a03db04c5cf
rpuszkin <130287042+rpuszkin@users.noreply.github.com>
2024-09-03 18:21:25 +0200

    script now checking that data from API is arry

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ document.getElementById("searchKitty").addEventListener("click", function () {
   fetch("https://api.thecatapi.com/v1/images/search")
     .then((response) => response.json())
     .then((data) => {
-      if (data.length > 0) {
+      if (Array.isArray(data) && data.length > 0) {
         const imgUrl = data[0].url;
         const imgElement = document.createElement("img");
         imgElement.src = imgUrl;
